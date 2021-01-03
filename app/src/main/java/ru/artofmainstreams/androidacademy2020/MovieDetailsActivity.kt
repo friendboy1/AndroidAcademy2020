abbd0a8021898701078c28f87b0c7b8bbb58a6c3
Andrei Khromov <friendboy@yandex.ru>
2021-01-03 04:37:11 +0300

    Homework #2

---
+++ app/src/main/java/ru/artofmainstreams/androidacademy2020/MovieDetailsActivity.kt
@@ -0,0 +1,16 @@
+package ru.artofmainstreams.androidacademy2020
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Экран детальной информации о фильме
+ *
+ * @author Andrei Khromov on 16.11.2020
+ */
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
